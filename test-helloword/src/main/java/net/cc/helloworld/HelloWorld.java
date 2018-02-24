63d18b0625b535b423740013bdfbba2ea2756ae2
zhizhonghua <zhizhonghua@boco.com.cn>
2018-02-24 10:13:50 +0800

    1. 修改腐工程 pom 文件 2. 新增test-helloword 工程

---
+++ test-helloword/src/main/java/net/cc/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package net.cc.helloworld;
+
+/**
+ * @author zhonghua.zhi
+ * Created by 2018/2/24 - 上午10:12
+ * ************************************
+ * Project Name first
+ * Package Name net.cc.helloworld
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("hello world !");
+    }
+}
