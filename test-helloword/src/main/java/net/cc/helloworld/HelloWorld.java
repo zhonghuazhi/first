f28e13b1315b0a94ba4200f835de67279ad0ab90
zhizhonghua <zhizhonghua@boco.com.cn>
2018-02-24 12:31:19 +0800

    test-helloword 项目 1. 新增DemoDate类

--- test-helloword/src/main/java/net/cc/helloworld/HelloWorld.java
+++ test-helloword/src/main/java/net/cc/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package net.cc.helloworld;
 
+import net.cc.helloworld.Date.DemoDate;
+
 /**
  * @author zhonghua.zhi
  * Created by 2018/2/24 - 上午10:12
@@ -11,5 +13,6 @@ public class HelloWorld {
 
     public static void main(String[] args){
         System.out.println("hello world !");
+        DemoDate.getSystemDate();
     }
 }
